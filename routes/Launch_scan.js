624b269ba838e0659f769a7e4ba4007fd20664c0
Cosme Abraham Ozuna Castillo <cosme.ozuna@softtekge.com>
2018-01-29 15:52:26 -0600

    modificando code

--- routes/Launch_scan.js
+++ routes/Launch_scan.js
@@ -9,17 +9,18 @@ router.get('/', function(req, res, next) {
   con = '';
   console.log('Entra');
   for (var i = 10; i < a.length-2; i++) {
-    console.log('Tamaño de a: ' + a.length);
-    console.log('Esto es lo que trae, '+ a + ' ' + a[i] + ' ' + a.token);
     con += a[i];
   }
 
+  //POST DATA
   console.log('\n Token' + a.token);
   jsonObject2 = JSON.stringify({
       uuid: 'ab4bacd2-05f6-425c-9d79-3ba3940ad1c24e51e1f403febe40',
       settings: {"emails":"","filter_type":"and","filters":[],"launch_now":true,"enabled":false,"file_targets":"","text_targets":"3.211.65.171","policy_id":"6749","scanner_id":"1","folder_id":13628,"description":"test2","name":"test2"}
     });
     console.log('x-cookie: ' +con);
+
+  //CREATE SCAN HEADERS
   let postheaders = {
       'Content-Type' : 'application/json',
       'Content-Length' : Buffer.byteLength(jsonObject2, 'utf8'),
@@ -27,6 +28,7 @@ router.get('/', function(req, res, next) {
       'x-Cookie':  'token='+con
   };
 
+  //CREATE OPTIONS TO CONNECT IN ORDER TO RUN A SCAN
   let options = {
   	hostname: '3.8.86.49',
   	port: '8834',
