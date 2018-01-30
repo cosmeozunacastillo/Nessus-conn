709387e3460574c531e29976586b641247ba3c34
Cosme Abraham Ozuna Castillo <cosme.ozuna@softtekge.com>
2018-01-30 11:40:02 -0600

    Subiendo ultimos cambios

--- routes/Launch_scan.js
+++ routes/Launch_scan.js
@@ -6,10 +6,10 @@ var connection = require('../Files/conn2');
 
 router.get('/', function(req, res, next) {
   //OBTAIN token
-  token_con = ''; // WHERE THE TOKEN WILL BE STORED
+  con = ''; // WHERE THE TOKEN WILL BE STORED
   console.log('Entra');
   for (var i = 10; i < a.length-2; i++) {
-    token_con += a[i];
+    con += a[i];
   }
 
   //POST DATA
@@ -18,14 +18,14 @@ router.get('/', function(req, res, next) {
       uuid: 'ab4bacd2-05f6-425c-9d79-3ba3940ad1c24e51e1f403febe40',
       settings: {"emails":"","filter_type":"and","filters":[],"launch_now":true,"enabled":false,"file_targets":"","text_targets":"3.211.65.171","policy_id":"6749","scanner_id":"1","folder_id":2,"description":"test23","name":"test2"}
     });
-    console.log('x-cookie: ' +token_con);
+    console.log('x-cookie: ' +con);
 
   //CREATE SCAN HEADERS
   let postheaders = {
       'Content-Type' : 'application/json',
       'Content-Length' : Buffer.byteLength(jsonObject2, 'utf8'),
   		'rejectUnauthorized': false,
-      'x-Cookie':  'token='+token_con
+      'x-Cookie':  'token='+con
   };
 
   //CREATE OPTIONS TO CONNECT IN ORDER TO RUN A SCAN
@@ -46,7 +46,7 @@ router.get('/', function(req, res, next) {
     console.log(err);
   });
 
-  res.send('Aqui esta solo el token: ' + token_con);
+  res.send('Aqui esta solo el token: ' + con);
 });
 
 module.exports = router;
