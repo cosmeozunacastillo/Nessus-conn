ca765322737bb2c323becc1800dbcc2a35877d01
Oscar <nazzguler@hotmail.com>
2018-01-31 15:05:57 -0600

    scan status

--- routes/Launch_scan.js
+++ routes/Launch_scan.js
@@ -11,16 +11,27 @@ router.get('/', function(req, res, next) {
   console.log('\n Token' + a.token);
   let jsonObject2 = JSON.stringify({
       uuid: 'ab4bacd2-05f6-425c-9d79-3ba3940ad1c24e51e1f403febe40',
-      settings: {"emails":"","filter_type":"and","filters":[],"launch_now":true,"enabled":false,"file_targets":"","text_targets":"3.211.65.171","policy_id":"6749","scanner_id":"1","folder_id":2,"description":"test23","name":"test2"}
+      settings: {"emails":"",
+                  "filter_type":"and",
+                  "filters":[],
+                  "launch_now":true,
+                  "enabled":false,
+                  "file_targets":"",
+                  "text_targets":"0.0.0.0",
+                  "policy_id":"6749",
+                  "scanner_id":"1",
+                  "folder_id":1,
+                  "description":"test23",
+                  "name":"test5"}
     });
-    console.log('x-cookie: ' +con);
+    console.log('x-cookie: ' +a);
 
   //CREATE SCAN HEADERS
   let postheaders = {
       'Content-Type' : 'application/json',
       'Content-Length' : Buffer.byteLength(jsonObject2, 'utf8'),
   		'rejectUnauthorized': false,
-      'x-Cookie':  'token='+con
+      'x-Cookie':  'token='+a
   };
 
   //CREATE OPTIONS TO CONNECT IN ORDER TO RUN A SCAN
@@ -37,11 +48,13 @@ router.get('/', function(req, res, next) {
   connection.post('https://3.8.86.49:8834/scans',jsonObject2,options).then(result => {
   	console.log('\n Body cuando pone a correr el scanner : '+result.body);
     console.log('\n Tamaño del JSON Body : '+result.body.length);
+    scannerInfo.id = JSON.parse(result.body).scan.id;
+    console.log("Sending scan, the id is: "+scannerInfo.id);
+    res.send(JSON.parse(result.body));
   }).catch(function(err){
     console.log(err);
   });
 
-  res.send('Aqui esta solo el token: ' + con);
 });
 
 module.exports = router;
--- routes/getScanStatus.js
+++ routes/getScanStatus.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var connection = require('../Files/conn2');
+var watch = require('node-watch');
 aa = {};
 
+scannerInfo = {
+    "id": null,
+    "status":null,
+};
 router.get('/', function(req, res, next) {
     //CREATING FOLDER ID
     folder = 13628;
@@ -28,11 +33,37 @@ router.get('/', function(req, res, next) {
         result => {
           aa = result.body;
           //console.log(typeof(JSON.parse(result.body)));
-          console.log(JSON.parse(result.body).folders);
-          res.send('This is the response: ' + aa);
+          console.log(JSON.parse(aa).scans);
+          //res.send('This is the response: ' + aa);
+          //res.send(JSON.parse(result.body).scans[0]);
     }).catch(function(err){
     console.log(err);
     });
+    while(scannerInfo.status!="completed"){
+        
+        connection.get('https://3.8.86.49:8834/scans?folder_id='+folder,options).then(
+        result => {
+            //console.log("hola");
+            aa = JSON.parse(result.body).scans;
+            //console.log(aa.length);
+            entire_loop:
+            for(var i=0; i<aa.length; i++){
+                for(var key in aa[i]){
+                    //alert(aa[i][key]);
+                    console.log("This is the key: "+key+" and this is the value of the key: "+aa[i][key]);
+                    if(key=='id' && aa[i][key]==scannerInfo.id){
+                        
+                        scannerInfo.status = aa[i];
+                        break entire_loop;
+                    }
+                }
+            }
+
+        }).catch(function(err){
+        console.log(err);
+        });
+    }
+    console.log("scanner completed!");
     //res.send('This is the response: ' + aa);
 });
 
