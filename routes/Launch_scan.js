3c1b26fd2feebd74305b38ede5f17a85ef6bcb84
Juan Arturo Gonzalez Araiza <juan.gonzaleza@softtekge.com>
2018-01-31 15:02:35 -0600

    Arreglando el launc

--- routes/Launch_scan.js
+++ routes/Launch_scan.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../Files/conn2');
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a0193c9683ad5d59c60f1ac6bd9bca168da40429
 /* GET users listing. */
 
 router.get('/', function(req, res, next) {
@@ -15,7 +11,6 @@ router.get('/', function(req, res, next) {
   console.log('\n Token' + a.token);
   let jsonObject2 = JSON.stringify({
       uuid: 'ab4bacd2-05f6-425c-9d79-3ba3940ad1c24e51e1f403febe40',
-<<<<<<< HEAD
       settings: {"emails":"",
       "filter_type":"and",
       "filters":[],
@@ -30,22 +25,13 @@ router.get('/', function(req, res, next) {
       "name":ScannerName}
     });
     console.log('x-cookie: ' +a);
-=======
-      settings: {"emails":"","filter_type":"and","filters":[],"launch_now":true,"enabled":false,"file_targets":"","text_targets":"3.211.65.171","policy_id":"6749","scanner_id":"1","folder_id":2,"description":"test23","name":"test2"}
-    });
-    console.log('x-cookie: ' +con);
->>>>>>> a0193c9683ad5d59c60f1ac6bd9bca168da40429
 
   //CREATE SCAN HEADERS
   let postheaders = {
       'Content-Type' : 'application/json',
       'Content-Length' : Buffer.byteLength(jsonObject2, 'utf8'),
   		'rejectUnauthorized': false,
-<<<<<<< HEAD
       'x-Cookie':  'token='+a
-=======
-      'x-Cookie':  'token='+con
->>>>>>> a0193c9683ad5d59c60f1ac6bd9bca168da40429
   };
 
   //CREATE OPTIONS TO CONNECT IN ORDER TO RUN A SCAN
@@ -66,11 +52,7 @@ router.get('/', function(req, res, next) {
     console.log(err);
   });
 
-<<<<<<< HEAD
   res.send('Aqui esta solo el token: ' + a);
-=======
-  res.send('Aqui esta solo el token: ' + con);
->>>>>>> a0193c9683ad5d59c60f1ac6bd9bca168da40429
 });
 
 module.exports = router;
