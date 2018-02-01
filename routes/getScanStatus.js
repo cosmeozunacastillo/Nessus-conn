406eeac3eabd264f868799f066f1cffdd49e535e
Oscar <nazzguler@hotmail.com>
2018-02-01 10:49:06 -0600

    getStatus working as API

--- routes/getScanStatus.js
+++ routes/getScanStatus.js
@@ -4,17 +4,13 @@ var connection = require('../Files/conn2');
 var watch = require('node-watch');
 aa = {};
 
-scannerInfo = {
-    "id": null,
-    "status":null,
-};
-
-finished = '';
-
 router.get('/', function(req, res, next) {
-    //CREATING FOLDER ID
-    folder = 13628;
-    //CREATE SCAN HEADERS
+
+    var folder = (typeof(req.query.folder) == 'undefined'|| req.query.folder=='')?13628:req.query.folder;
+    //console.log(folder);
+    var scan = req.query.scan;
+    //console.log(scan);
+
     let postheaders = {
         //'Content-Type' : 'application/json',
         //'Content-Length' : Buffer.byteLength(jsonObject2, 'utf8'),
@@ -22,77 +18,47 @@ router.get('/', function(req, res, next) {
         'x-Cookie':  'token='+ a,
         //'Host': '3.8.86.49:8834'
     };
-  //CREATE OPTIONS TO REQUEST STATUS
-    options = {
+
+    let options = {
         host: '3.8.86.49',
         port: '8834',
         path: '/scans?folder_id='+folder,
         method: 'GET',
         rejectUnauthorized: false,
         headers: postheaders,
-        tryToDecode: true // Tries to decode gzip
+        tryToDecode: true 
     }
+
     connection.get('https://3.8.86.49:8834/scans?folder_id='+folder,options).then(
         result => {
-          //aa = result.body;
-          //console.log(typeof(JSON.parse(result.body)));
-<<<<<<< HEAD
-          //console.log(JSON.parse(aa).scans);
-          //res.send('This is the response: ' + aa);
-          res.send(JSON.parse(result.body).scans);
-=======
-          aa = JSON.parse(result.body).scans;
-          //console.log(JSON.parse(aa).scans);
-          //console.log(aa);
-          res.send(' Id del scanner corriendo : ' + aa[0].id);
-          //res.send(JSON.parse(result.body).scans[0]);
-          console.log('Tamaño del array de aa: ' + aa.length);
+            aa = JSON.parse(result.body).scans;
+            console.log(aa);
+            var response = {};
+            var wasScanFound = false;
+            
+            if(aa != null){
+                if(typeof(scan)=='undefined' || scan==''){
+                    res.send(aa);
+                }else{
 
-          //while(finished == ''){
-          for (var i = 0; i < aa.length; i++) {
-            if (aa[i].id == scannerInfo.id){
-                console.log('Scanner ' + scannerInfo.id + ' Encontrado, ahora se procedera a checar el estatus... ');
-                if (aa[i].status != 'completed' && aa[i].status != 'cancelled' && aa[i].status != 'paused'){
-                  console.log('Scan '+ aa[i].status +' , PLEASE wait.....!!!!!');
-                }else {
-                  console.log('Scan ' + aa[i].status);
-                  finished = 'completed';
+                    for (var i = 0; i < aa.length; i++) {
+                        if (aa[i].id == scan){
+                            res.send(aa[i]);
+                            wasScanFound = true;
+                            console.log(typeof(aa[i]));
+                            break;
+                        }
+                    }
+                    if(!wasScanFound)
+                        res.send(JSON.parse('{"error":"provided scan was not found."}'));
                 }
+            }else{
+                res.send(JSON.parse('{"error":"provided folder was not found."}'));
             }
-          }
-        //}
-
->>>>>>> d1ad9206dc4486ebd895a05e1c6ea7529efc5593
     }).catch(function(err){
     console.log(err);
     });
 
-    /*
-    while(scannerInfo.status!="completed"){
-
-        connection.get('https://3.8.86.49:8834/scans?folder_id='+folder,options).then(
-        result => {
-            console.log("hola");
-            aa = JSON.parse(result.body).scans;
-            console.log(aa.length);
-            entire_loop:
-            for(var i=0; i<aa.length; i++){
-                for(var key in aa[i]){
-                    //alert(aa[i][key]);
-                    console.log("This is the key: "+key+" and this is the value of the key: "+aa[i][key]);
-                    if(key=='id' && aa[i][key]==scannerInfo.id){
-
-                        scannerInfo.status = aa[i];
-                        break entire_loop;
-                    }
-                }
-            }
-        }).catch(function(err){
-        console.log(err);
-        });
-    }*/
-    //console.log("scanner completed!");
-    //res.send('This is the response: ' + aa);
 });
 
 module.exports = router;
