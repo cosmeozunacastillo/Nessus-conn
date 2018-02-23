9cf9fbe3f087b900ce7ed883d6fd0fbe76951e97
Oscar <nazzguler@hotmail.com>
2018-02-23 07:38:22 -0600

    nessus api returns vulnerability report already parsed from xml to json

--- routes/nessus/getScanVulnerabilities.js
+++ routes/nessus/getScanVulnerabilities.js
@@ -34,10 +34,6 @@ router.get('/', function(req, res, next) {
                         let allScans = {};
                         allScans = JSON.parse(body);
                         asssessmentId = 0;
-                        //console.log(a);
-                        //console.log(allScans);
-                        //console.log("making first request");
-                        //console.log(allScans);
                         entire_loop:
                         for(var i=0; i<allScans.scans.length; i++){
                             //console.log(allScans[i]);
@@ -61,6 +57,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/', function(req, res, next) {
+    //console.log("2nd request body:"+req.query);console.log(req);
     request.get({
         url:'https://3.8.86.49:8834/scans/'+asssessmentId,
         headers: {
@@ -69,9 +66,72 @@ router.get('/', function(req, res, next) {
         method:'GET',
         rejectUnauthorized: false
         },(error,response,body) => {
+            let scanDetails = {};
+            scanDetatils = JSON.parse(body);
+            historyId = (scanDetatils.history)?scanDetatils.history[0].history_id:-1;
+            //console.log(historyId);
             //console.log("making second request");
-            res.send(JSON.parse(body));
+            next();
+            //res.send(JSON.parse(body));
         });
 });
+
+router.get('/', function(req, res, next) {
+    //console.log("3rd request body:"+req.body);
+    request.post({
+        url:'https://3.8.86.49:8834/scans/'+asssessmentId+'/export?history_id='+historyId,
+        headers: {
+            'Content-Type' : 'application/json',
+            'rejectUnauthorized': false,
+            'X-Cookie':  'token='+ a},
+        body: JSON.stringify({'format':'nessus'}),
+        rejectUnauthorized: false
+        },(error,response,body) => {
+            //console.log(urlExport);
+            //console.log("making second request");
+            fileId = JSON.parse(body).file;
+            next();
+        });
+});
+
+router.get('/', function(req, res, next) {
+    //console.log("4th request body:"+req.body);
+    urlExport = 'https://3.8.86.49:8834/scans/'+asssessmentId+'/export/'+fileId+'/download';
+    request.get({
+        url:urlExport,
+        headers: {
+            'rejectUnauthorized': false,
+            'X-Cookie':  'token='+ a,
+            'Content-Type' : 'application/json'
+        },
+        rejectUnauthorized: false
+        },(error,response,body) => {
+            var converter = require('xml2js');
+            var p = new converter.Parser();
+
+
+            p.parseString(body, function (err, result) {
+                var s = JSON.stringify(result,undefined,3);
+                var t = JSON.parse(s);
+                //console.log('Result: \n' + s + '\n');
+                let untranslatedVulnerabilities = {
+                    'assessment':(t)?t.NessusClientData_v2.Report[0].$.name:-1,
+                    'policy':(t)?t.NessusClientData_v2.Policy:-1,
+                    'hosts':[]
+                };
+                for(var i=0; i<t.NessusClientData_v2.Report[0].ReportHost.length; i++){
+                    //console.log(t.NessusClientData_v2.Report[0].ReportHost[i]);
+                    untranslatedVulnerabilities.hosts.push(t.NessusClientData_v2.Report[0].ReportHost[i]);
+                }
+                //console.log(untranslatedVulnerabilities);
+                //res.send(s);
+                res.send(untranslatedVulnerabilities);
+            });
+            //console.log(urlExport);
+            //console.log("making second request");
+            //next();
+            //res.send(body);
+        });
+})
 
 module.exports = router;
--- routes/nessus/validateToken.js
+++ routes/nessus/validateToken.js
@@ -11,18 +11,27 @@ router.get('/', function(req, res, next) {
       'rejectUnauthorized': false
     },
     url: 'https://3.8.86.49:8834/session',
-    method:'get'
+    method:'GET',
+    rejectUnauthorized: false
     },
     (error,response,body) => {
       console.log('checking token status...');
-      if (body == '{"error":"Invalid Credentials"}' 
-        || body == '{"error":"You need to log in to perform this request"}') {
-        console.log('expired token, reconnecting... ');
-        res.send(JSON.parse('{"status":"token has expired, a new one will be requested"}'));
-        return validation.authNessus();
-      }else{
+
+      switch(true){
+        case error!=null:
+          console.log('connection error: '+error.code);
+          var connError = {"error":"connection error","code":error.code};
+          res.send(connError);
+        break;
+        case JSON.parse(body).error!=null:
+          console.log('invalid token, reconnecting... ');
+          res.send(JSON.parse('{"status":"invalid","message": "a new one will be requested"}'));
+          return validation.authNessus();
+        break;
+        default:
         console.log('current token is valid');
-        res.send(JSON.parse('{"status":"current token is valid"}'));
+        res.send(JSON.parse('{"status":"valid","message":"current token is valid","token":"'+a+'"}'));
+        break;
       }
     }
   );
